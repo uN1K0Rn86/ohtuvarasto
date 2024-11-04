7c1022044a874ef810caff6a4cab0af0d088da6a
Sami Heinimo <sami.heinimo@helsinki.fi>
2024-11-04 14:40:46 +0200

    Added 100% branch coverage for tests for the class Varasto.

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -13,16 +13,34 @@ class TestVarasto(unittest.TestCase):
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
 
+    def test_negatiivinen_tilavuus(self):
+        self.neg_tilavuus = Varasto(-2)
+        self.assertAlmostEqual(self.neg_tilavuus.tilavuus, 0)
+
+    def test_negatiivinen_alkusaldo(self):
+        self.neg_varasto = Varasto(10, -2)
+        self.assertAlmostEqual(self.neg_varasto.saldo, 0)
+
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
 
         self.assertAlmostEqual(self.varasto.saldo, 8)
+
+    def test_negatiivinen_lisays(self):
+        saldo = self.varasto.saldo
+        self.varasto.lisaa_varastoon(-4)
+        self.assertAlmostEqual(saldo, self.varasto.saldo)
 
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
         self.varasto.lisaa_varastoon(8)
 
         # vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 2)
+
+    def test_lisays_liikaa(self):
+        self.varasto.lisaa_varastoon(15)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
 
     def test_ottaminen_palauttaa_oikean_maaran(self):
         self.varasto.lisaa_varastoon(8)
@@ -38,3 +56,17 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_ota_negatiivinen_maara(self):
+        saldo = self.varasto.saldo
+        self.varasto.ota_varastosta(-4)
+        self.assertAlmostEqual(saldo, self.varasto.saldo)
+
+    def test_ota_kaikki(self):
+        self.varasto.lisaa_varastoon(8)
+        kaikki = self.varasto.ota_varastosta(15)
+        self.assertAlmostEqual(kaikki, 8)
+
+    def test_string_method(self):
+        varasto = str(self.varasto)
+        self.assertEqual(varasto, "saldo = 0, vielä tilaa 10")
